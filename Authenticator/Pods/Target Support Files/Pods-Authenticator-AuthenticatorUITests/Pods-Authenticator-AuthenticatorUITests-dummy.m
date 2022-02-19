ebf6b967f77dba209313a492576f8a71f771bda4
tjy-dev <tjydeveloper@gmail.com>
2022-02-19 17:32:03 +0900

    initial commit

--- Authenticator/Pods/Target Support Files/Pods-Authenticator-AuthenticatorUITests/Pods-Authenticator-AuthenticatorUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Authenticator_AuthenticatorUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Authenticator_AuthenticatorUITests
-@end
